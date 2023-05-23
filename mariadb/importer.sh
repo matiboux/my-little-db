bade8dc2739b5a0398e4d1999e7ca4d775e71ed0
Mati <matiboux@gmail.com>
2023-05-23 02:21:14 +0200

    Import multiple DBs, data in subdirectories (#2)

--- mariadb/importer.sh
+++ mariadb/importer.sh
@@ -31,4 +31,22 @@ for sql_file in ./data/*.sql; do
 	mysql $DB_ARGS --database="$DB_NAME" < "$sql_file"
 done
 
+# Import data from subdirectories in separate databases
+for dir in ./data/*/; do
+
+	# Use the directory name as the database name
+	dir_name=$(basename "$dir")
+
+	# Create the database
+	echo "Creating database $dir_name..."
+	mysql $DB_ARGS -e "CREATE DATABASE IF NOT EXISTS \`$dir_name\`;"
+
+	# Import all the SQL files in the directory
+	for sql_file in "$dir"*.sql; do
+		echo "Importing $sql_file..."
+		mysql $DB_ARGS --database="$dir_name" < "$sql_file"
+	done
+
+done
+
 echo "Database data import complete!"
